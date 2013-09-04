fa5e9150274340070210f790cdaf70d712c50552
Kevin Aditya <k_vin92@yahoo.com>
2013-09-04 11:48:01 +0000

    make a basic user model with secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true
   has_many :microposts
+  has_secure_password
+  validates :password, length: { minimum: 6}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130903143827) do
+ActiveRecord::Schema.define(version: 20130904105932) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20130903143827) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
